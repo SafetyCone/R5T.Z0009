6220d50d90d9f64747e5b27cc2d25327fc5222b8
davidcoats <david@getsafetycone.com>
2024-09-22 20:20:40 -0700

    Interim commit.

--- source/R5T.Z0009/Code/Values/IProjectTypeIdentities.cs
+++ source/R5T.Z0009/Code/Values/IProjectTypeIdentities.cs
@@ -1,6 +1,7 @@
 using System;
 
 using R5T.T0131;
+using R5T.T0143;
 
 
 namespace R5T.Z0009
@@ -9,7 +10,10 @@ namespace R5T.Z0009
 	public partial interface IProjectTypeIdentities : IValuesMarker
 	{
 #pragma warning disable IDE1006 // Naming Styles
+
+        [Ignore]
         private static IProjectTypeIdentityStrings _Strings { get; } = ProjectTypeIdentityStrings.Instance;
+
 #pragma warning restore IDE1006 // Naming Styles
 
 
